71460bb149fde7bc942c6a013fed8d0d258d158e
JBLDSKY <JBLDSKY@gmail.com>
2024-05-11 19:50:30 +0200

    first couple tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use jaas::configuration::get_configuration;
+use jaas::startup::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let config = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", config.application_port);
+
+    let listener = TcpListener::bind(address).expect("Failed to bind to random port");
+
+    run(listener)?.await
 }
