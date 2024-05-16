ee8ad9b80491809e85ce46c8664786198f9e1004
JBLDSKY <JBLDSKY@gmail.com>
2024-05-16 22:50:10 +0200

    add Secrecy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use jaas::configuration::get_configuration;
 use jaas::startup::run;
 use jaas::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -11,9 +12,8 @@ async fn main() -> Result<(), std::io::Error> {
 
     let config = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", config.application_port);
-    let connection_string = config.database.connection_string();
 
-    let pool = PgPool::connect(&connection_string)
+    let pool = PgPool::connect(config.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
