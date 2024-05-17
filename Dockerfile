409f8a53530b9a39fe2b98a1c802aaf73507f2ca
JBLDSKY <JBLDSKY@gmail.com>
2024-05-17 19:30:18 +0200

    Dockerizeeeed

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
-FROM rust:1.71.0
+FROM rust:1.78.0
 
 WORKDIR /app
-RUN apt update &&apt install lld clang -y
+RUN apt update && apt install lld clang -y
 COPY . .
 ENV SQLX_OFFLINE true
+ENV APP_ENVIRONMENT production
 RUN cargo build --release
 ENTRYPOINT ["./target/release/jaas"]
 
--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,9 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration.");
-    let address = format!("127.0.0.1:{}", config.application_port);
+    let address = format!("{}:{}", config.application.host, config.application.port);
 
-    let pool = PgPool::connect(config.database.connection_string().expose_secret())
-        .await
+    let pool = PgPool::connect_lazy(config.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
 
     let listener = TcpListener::bind(address).expect("Failed to bind to random port");
